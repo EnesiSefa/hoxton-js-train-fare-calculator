1e6f329dff8c8457cd0c2668f47ff2e34c271cc4
enesi sefa <enesisefa380@gmail.com>
2022-06-14 18:10:00 +0200

    ...

--- index.js
+++ index.js
@@ -9,14 +9,25 @@
 // the user how much they will need to pay for the ride.
 
 
+//Challenge
+//Rewrite your program so that the user chooses a starting point 
+//and a destination from a list of stations, 
+//besides inputting their age, and then get the ticket price. 
+
+//You can come up with the stations' names and distances. 
+//Try also to code it so 
+//that you only need to change the price per km in one place, 
+//and all the stations should have the updated price
 
 
 
 
+
+// basic 
 let age = Number(prompt("what is your age?"))
 const pricePerKm = 0.21
-const under18Discount = 0.2 
-const over65Discount = 0.4 
+const under18Discount = 0.8 
+const over65Discount = 0.6 
 let travelDistance = Number(prompt("How many km you'd like to travel"))
 
 let travelPrice = travelDistance * pricePerKm
@@ -24,23 +35,27 @@ let travelPrice = travelDistance * pricePerKm
  
 
 let totalPay
-    if( age < 18){
- totalPay =  travelPrice * (1 -under18Discount)
- console.log("You will pay: " +"£" + totalPay)
+    if( age < 18 ){
+ totalPay =  travelPrice * under18Discount
+ console.log("for You will pay: " +"£" + totalPay.toFixed(2))
 } 
 else if( age > 65) {
-    totalPay =  travelPrice * (1-over65Discount)
-    console.log("You will pay: " +"£" + totalPay)
+    totalPay =  travelPrice * over65Discount
+    console.log("You will pay: " +"£" + totalPay.toFixed(2))
 
 } 
 else {
 
-    console.log("You will pay: " +"£" + travelPrice)
+    console.log("You will pay: " +"£" + travelPrice.toFixed(2))
 }
     
 
 
+// challenge
 
+if(travelDistance = 5){
+    console.log("first station")
+}
 
 
 
